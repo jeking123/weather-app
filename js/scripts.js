f9a1ef6cd1285026dcc98d2d51c30eae15c80926
jeking123 <azunaki1@gmail.com>
2016-12-01 13:42:15 -0800

    Multiple Cities

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,41 @@
-// My Scripts
+// Spokane
+$.simpleWeather({
+    location: 'Spokane',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.spokane .temp').text(weather.temp);
+      $('.spokane .city').text(weather.city);
+      $('.spokane img').attr('src', weather.image);
+      $('.spokane .title').text(weather.title);
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      $('body').append('<p>No Weather Found!</p>');
+    }
+  
+  });
+
+// Seattle
+$.simpleWeather({
+    location: '98105',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.seattle .temp').text(weather.temp);
+      $('.seattle .city').text(weather.city);
+      $('.seattle img').attr('src', weather.image);
+      $('.seattle .title').text(weather.title);
+      
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      $('body').append('<p>No Weather Found!</p>');
+    }
+  
+  });
