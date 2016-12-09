41548f62865c4f0c85703fb9f3d71a7e3c2ed7bf
jeking123 <azunaki1@gmail.com>
2016-12-08 19:19:19 -0800

    Updated Weather App
    Functioning, added content, designed.

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,22 @@ $.simpleWeather({
       // Display Data
       $('.spokane .temp').text(weather.temp);
       $('.spokane .city').text(weather.city);
-      $('.spokane img').attr('src', weather.image);
+      $('.spokane .high').text(weather.high);
+      $('.spokane .low').text(weather.low);
       $('.spokane .title').text(weather.title);
-      
+      $('.spokane i').addClass( 'wi-' + weather.code );
+        $('.spokane .currently').text( weather.currently);
+        $('.spokane .day').text( weather.forecast[0].day);
+        $('.spokane .date').text( weather.forecast[0].date);
+        
+        
+        //Forecast
+        $('.spokane .forecast .date').text(weather.forecast[1].date);
+        $('.spokane .forecast .day').text(weather.forecast[1].day);
+        $('.spokane .forecast .high').text(weather.forecast[1].high);
+        $('.spokane .forecast .low').text(weather.forecast[1].low);
+          
+        
       // Entire weather object
       console.log(weather);
     },
@@ -28,16 +41,55 @@ $.simpleWeather({
       // Display Data
       $('.seattle .temp').text(weather.temp);
       $('.seattle .city').text(weather.city);
-      $('.seattle img').attr('src', weather.image);
+      $('.seattle .high').text(weather.high);
+      $('.seattle .low').text(weather.low);
       $('.seattle .title').text(weather.title);
-      
-      // If this condition so this icon
-        if( 26 >= weather.code && 30 <= weather.code ){
-            
-            $('.spokane .wi').addClass('wi-day-cloudy');
-            
-        }
+      $('.seattle i').addClass( 'wi-' + weather.code );
+        $('.seattle .currently').text( weather.currently);
+        $('.seattle .day').text( weather.forecast[0].day);
+        $('.seattle .date').text( weather.forecast[0].date);
         
+        
+        //Forecast
+        $('.seattle .forecast .day').text(weather.forecast[1].day);
+        $('.seattle .forecast .date').text(weather.forecast[1].date);
+        $('.seattle .forecast .high').text(weather.forecast[1].high);
+        $('.seattle .forecast .low').text(weather.forecast[1].low);
+        
+     
+        
+      // Entire weather object
+      console.log(weather);
+    },
+    error: function(error) {
+      $('body').append('<p>No Weather Found!</p>');
+    }
+  
+  });
+
+// Seattle
+$.simpleWeather({
+    location: 'cheney',
+    unit: 'f',
+    success: function(weather) {
+      
+      // Display Data
+      $('.cheney .temp').text(weather.temp);
+      $('.cheney .city').text(weather.city);
+      $('.cheney .high').text(weather.high);
+      $('.cheney .low').text(weather.low);
+      $('.cheney .title').text(weather.title);
+      $('.cheney i').addClass( 'wi-' + weather.code );
+        $('.cheney .currently').text( weather.currently);
+        $('.cheney .day').text( weather.forecast[0].day);
+        $('.cheney .date').text( weather.forecast[0].date);
+        
+        
+        //Forecast
+        $('.cheney .forecast .day').text(weather.forecast[1].day);
+        $('.cheney .forecast .date').text(weather.forecast[1].date);
+        $('.cheney .forecast .high').text(weather.forecast[1].high);
+        $('.cheney .forecast .low').text(weather.forecast[1].low);
         
       // Entire weather object
       console.log(weather);
@@ -54,6 +106,7 @@ $('.geo button').click( function() {
     //load weather using your lat/lng coordinates
     navigator.geolocation.getCurrentPosition(function(position) {
         getWeather(position.coords.latitude+','+position.coords.longitude); 
+        $('.geo button').hide();
     });
    
 });
@@ -70,8 +123,22 @@ var getWeather = function(location) {
         // Display Data
       $('.geo .temp').text(weather.temp);
       $('.geo .city').text(weather.city);
-      $('.geo img').attr('src', weather.image);
+      $('.geo .high').text(weather.high);
+      $('.geo .low').text(weather.low);
       $('.geo .title').text(weather.title);
+      $('.geo .code').text(weather.code);
+      $('.geo i').addClass( 'wi-' + weather.code );
+        
+        $('.geo .currently').text( weather.currently);
+        $('.geo .day').text( weather.forecast[0].day);
+        $('.geo .date').text( weather.forecast[0].date);
+        
+        
+        //Forecast
+        $('.geo .forecast .day').text(weather.forecast[1].day);
+        $('.geo .forecast .date').text(weather.forecast[1].date);
+        $('.geo .forecast .high').text(weather.forecast[1].high);
+        $('.geo .forecast .low').text(weather.forecast[1].low);
       
       // Entire weather object
       console.log(weather);
@@ -84,3 +151,5 @@ var getWeather = function(location) {
   });
     
 };
+
+// Applying Weather Codes
